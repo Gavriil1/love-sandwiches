49a941e816e646bfd7bd57e8f19c3d0a713734d4
Gavriil1 G <airbnbguide@gmail.com>
2023-05-26 10:58:12 +0000

    Retrive latest stock numbers from spreadsheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,48 +13,74 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
-
-data = sales.get_all_values()
-
-print(data)
-
 
 def get_sales_data():
     """
-        Get sales figures input from the user
+    Get sales figures input from the user.
     """
     while True:
-        print('Please enter sales  data from the last market')
-        print('Data should be six number, separated by commas.')
-        print('Example: 10, 20, 30, 40, 50, 60\n')
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-        data_str =  input("Enter your data here!\n")
+        data_str = input("Enter your data here: ")
+
         sales_data = data_str.split(",")
-        validate_data(sales_data)
 
         if validate_data(sales_data):
-            print("Data is valid")
+            print("Data is valid!")
             break
+
+    return sales_data
 
 
 def validate_data(values):
     """
-    Inside the try, converts all string values into integers,
+    Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
-    or if ther aren't exactly 6 values
+    or if there aren't exactly 6 values.
     """
-    print(values)
     try:
         [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-                f"Exactly 6 values required, our provided {len(values)}"
+                f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
-        print(f"invalid data: {e}, please try again.\n" )
+        print(f"Invalid data: {e}, please try again.\n")
         return False
 
     return True
 
-get_sales_data()
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated succesfully.\n")
+
+def calculate_surplus_data(sales_row):
+    '''
+    Compaire sales with stock and caluate the surplus or ech etach tiem type
+    the surplus ids defined as the sale giure sbutracted from the stock:
+    Postivie suplus indifcates waste
+    Negative suplus indicates extra made when the stock was solved.
+    '''
+    print('Calculating surplus data...\n')
+    stock = SHEET.worksheet('stock').get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+
+def main():
+    '''
+    Run all program functions
+    '''
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+print('Welcome to Love Sandwiches Data Automation')
+main()
